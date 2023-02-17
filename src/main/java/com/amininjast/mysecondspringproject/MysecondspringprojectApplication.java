55d98313bf7dccf335456f935c88d5e0d7c3aca7
Amininjast <aminarad228@gmail.com>
2023-02-17 19:19:29 +0330

    First commit nothing happen

---
+++ src/main/java/com/amininjast/mysecondspringproject/MysecondspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.amininjast.mysecondspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecondspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysecondspringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amininjast/mysecondspringproject/MysecondspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amininjast.mysecondspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysecondspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
